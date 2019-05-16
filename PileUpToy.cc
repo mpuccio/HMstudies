9080fa139668841521916df63dd42a08bcdbb587
Maximiliano Puccio <maximiliano.puccio@cern.ch>
2019-05-16 18:21:59 +0200

    FIT rejects RO frames, not single vertices

--- PileUpToy.cc
+++ PileUpToy.cc
@@ -29,46 +29,19 @@ struct vertex {
   bool inBunchPU;
 };
 
-void FindPrimaryVertexResolution() {
-  TCanvas cv("pvResParamCv");
-  cv.SetLogx();
-  cv.SetGridy();
-  cv.SetGridx();
-  double res[4]{56.,34.,12.,1.2};
-  double mult[4]{3,10,54,4000};
-  TF1* resMultParam = new TF1("resMultParam","[0]+[1]/sqrt(x)",0.1,1010);
-  resMultParam->SetParLimits(0,0,10);
-  TGraph* resMult = new TGraph(4,mult,res);
-  resMult->SetTitle(";Number of tracklets contributing to the vertex;Vertex z resolution (#mum)");
-  resMult->Fit(resMultParam);
-  resMult->SetMarkerStyle(20);
-  resMult->SetMinimum(0);
-  resMult->Draw("AP");
-  cv.SaveAs("ResolutionParam.pdf");
-}
-
 void PileUpToy(const double itsRate, const int fitRejection) {
   gRandom->SetSeed(123456);
-  TF1 *nch = new TF1("nch", "exp(log([0]) + TMath::LnGamma([2]+x) -"        \
-  "TMath::LnGamma([2]) - TMath::LnGamma(x+1) + log([1] / ([1]+[2])) * x"    \
-  "+ log(1.0 + [1]/[2]) * -[2])");
-  nch->SetParNames("scaling", "averagen", "k");
-  // nch->SetParameters(0.657063, 23.720545, 2.530290); // 5500 GeV
-  // nch->SetParameters(0.595498, 29.178745, 2.827428); // 10500 GeV
-  nch->SetParameters(0.569988, 31.995152, 2.799755); // 14000 GeV
-  // nch->SetParameters(0.515791, 39.485306, 3.807981); // 27000 GeV
-  nch->SetRange(2, 400);
 
   TFile multFile("mult.root");
   TH1* multHist = (TH1*)multFile.Get("multHist");
 
   TF1* pvResolution = new TF1("pvResolution","([0]+[1]/sqrt(x)) * 1.e-4",0.1,1010);
   pvResolution->SetParameters(-0.157435, 99.3137);
-  pvResolution->Draw();
 
   TH1D* hNvert = new TH1D("hNvert",";Number of vertices per RO frame;Counts",21,-0.5,20.5);
   TH1D* hNvertBC = new TH1D("hNvertBC",";Number of vertices per BC;Counts",21,-0.5,20.5);
   TH1D* hMult = new TH1D("hMult","Total multiplicity;Generated multiplicity;Events",300,0.5,300.5);
+  TH1D* hSelectedMult = new TH1D("hSelectedMult","Event processed by the ITS reco;Generated multiplicity;Events",300,0.5,300.5);
   TH1D* hSelectedGood = new TH1D("hSelectedGood","Good triggers;Generated multiplicity;Events",300,0.5,300.5);
   TH1D* hSelectedFake = new TH1D("hSelectedFake","Pile-up of 2 vertices;Generated multiplicity;Events",300,0.5,300.5);
   TH1D* hSelectedFake3 = new TH1D("hSelectedFake3","Pile-up of 3 or more vertices;Generated multiplicity;Events",300,0.5,300.5);
@@ -76,6 +49,7 @@ void PileUpToy(const double itsRate, const int fitRejection) {
   TH1D* hSelectedFakeOOB = new TH1D("hSelectedFakeOOB","Fake triggers (OOB);Generated multiplicity;Events",300,0.5,300.5);
   TH1D* hSelectedHalf = new TH1D("hSelectedHalf","Fake triggers;Generated multiplicity;Events",300,0.5,300.5);
   hMult->SetLineColor(kBlack);
+  hSelectedMult->SetLineColor(kBlack);
   hSelectedFakeOOB->SetLineColor(kRed);
   hSelectedFakeOOB->SetFillColor(kRed);
   hSelectedFakeOOB->SetFillStyle(3345);
@@ -102,11 +76,13 @@ void PileUpToy(const double itsRate, const int fitRejection) {
 
   int triggeredROframe{0};
   int physicsTriggeredROframe{0};
+  int fitSelected{0};
   for (int iROframe{0}; iROframe < kNumberOfROframes; ++iROframe) {
     genVertices.clear();
     recVertices.clear();
     int nVert{0};
     bool interestingRO{false};
+    bool fitVETO{true};
     for (int iBC{0}; iBC < nBCperRO; ++iBC) {
       int nVertBC = gRandom->Poisson(kMu);
       for (int iVert{0}; iVert < nVertBC; ++iVert) {
@@ -126,13 +102,22 @@ void PileUpToy(const double itsRate, const int fitRejection) {
           physicsTriggeredROframe++;
           interestingRO = true;
         }
+        if (mult > fitRejection)
+          fitVETO = false;
       }
       nVert+=nVertBC;
       hNvertBC->Fill(nVertBC);
     }
     hNvert->Fill(nVert);
 
-   
+    if (fitVETO)
+      continue;
+    else
+      fitSelected++;
+
+    for (const auto& vert : genVertices)
+      hSelectedMult->Fill(vert.n);
+
     recVertices = genVertices;
 
     bool merging{false};
@@ -149,7 +134,6 @@ void PileUpToy(const double itsRate, const int fitRejection) {
           }
           hSigmaZall->Fill(10 * sigma);
           if (delta < kMinSeparation) {
-            if (recVertices[iVert].bc == recVertices[jVert].bc || (recVertices[iVert].n > fitRejection && recVertices[jVert].n > fitRejection)) {
               merging = true;
               recVertices[iVert].merged = true;
               recVertices[iVert].z = (recVertices[iVert].z * recVertices[iVert].n + recVertices[jVert].z * recVertices[jVert].n) / (recVertices[iVert].n + recVertices[jVert].n);
@@ -159,7 +143,6 @@ void PileUpToy(const double itsRate, const int fitRejection) {
               recVertices[iVert].ids.insert(recVertices[iVert].ids.end(), recVertices[jVert].ids.begin(), recVertices[jVert].ids.end());
               recVertices.erase(recVertices.begin()+jVert);
               break;
-            }
           }
         }
       }
@@ -200,7 +183,8 @@ void PileUpToy(const double itsRate, const int fitRejection) {
       triggeredROframe++;
   }
 
-  std::string fitString = fitRejection > 0 ? Form("FIT rejection: %i",fitRejection) : "No FIT rejection";
+  double fitRate = fitSelected / double(kNumberOfROframes) * itsRate;
+  std::string fitString = fitRejection > 0 ? Form("FIT selects N_{ch} > %i (%.1f kHz)",fitRejection, fitRate * 1.e-3) : "No FIT rejection";
   double triggerRate = triggeredROframe / double(kNumberOfROframes) * itsRate;
   double physicsRate = physicsTriggeredROframe / double(kNumberOfROframes) * itsRate;
 
@@ -208,11 +192,12 @@ void PileUpToy(const double itsRate, const int fitRejection) {
   TCanvas finalCv("finalCv");
   finalCv.cd();
   finalCv.SetLogy();
-  hMult->Draw();
+  hSelectedMult->Draw();
   hSelectedGood->Draw("same");
   hSelectedFake->Draw("same");
   hSelectedFake3->Draw("same");
-  TLegend* leg = finalCv.BuildLegend(0.517544,0.523478,0.997494,0.862609,Form("#splitline{ITS RO rate: %1.1f MHz, %s}{Trigger/Process rates: %.0f/%.0f Hz}",itsRate * 1.e-6,fitString.data(),triggerRate,physicsRate));
+  TLegend* leg = finalCv.BuildLegend(0.5,0.5,0.9,0.82,Form("#splitline{#splitline{ITS RO rate: %1.1f MHz}{%s}}{Trigger/Process rates: %.0f/%.0f Hz}",itsRate * 1.e-6,fitString.data(),triggerRate,physicsRate));
+  leg->SetMargin(0.15);
   leg->SetFillStyle(0);
   finalCv.SaveAs(Form("trigger_%.0f_%i.pdf",itsRate*1.e-3,fitRejection));
   finalCv.Write();
